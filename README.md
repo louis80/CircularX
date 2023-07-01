cd9759af73c3845752a487d0d38dc5590ac57a7c
louis.jeanpierre@sia-partners.com <56889786+louis80@users.noreply.github.com>
2023-07-01 22:31:01 +0200

    add db

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ Exercice complémentaire Python
 Le détail de la démarche proposée est résumé dans le powerpoint exercice_circularx.pptx
 
 ### Instruction pour lancer les notebooks 
-##### Création de l'environnement virtuel
+#### Création de l'environnement virtuel
 python -m venv .venv
 
-##### Activer l'environnement
+#### Activer l'environnement
 .\env_name\Scripts\activate # Windows
 source env_name/bin/activate # linux
 
-##### Installation des requirements
+#### Installation des requirements
 pip install -r requirements.txt
 python -m spacy download en_core_web_sm
--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,30 +1,50 @@
 
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS brand;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS sale;
+DROP TABLE IF EXISTS prod_feat;
+DROP TABLE IF EXISTS feature;
+
 CREATE TABLE IF NOT EXISTS product (
     product_id INT NOT NULL PRIMARY KEY,
     product_label VARCHAR(400) NOT NULL,
-    product_category VARCHAR(100),
     product_url VARCHAR(500), 
-    mean_price FLOAT
+    mean_price FLOAT,
+    -- 
+    brand_id INT, 
+    category_id INT 
 );
 
 CREATE TABLE IF NOT EXISTS brand (
     brand_id INT NOT NULL PRIMARY KEY,
-    product_id INT, 
     brand_label VARCHAR(400) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS feature (
-    feature_id INT NOT NULL PRIMARY KEY,
-    product_id INT, 
-    feature_label VARCHAR(400) NOT NULL,
-    feature_category VARCHAR(400)
+CREATE TABLE IF NOT EXISTS category (
+    category_id INT NOT NULL PRIMARY KEY,
+    category_label VARCHAR(400) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS sale (
     sale_id INT NOT NULL PRIMARY KEY,
-    product_id INT, 
     sale_label VARCHAR(400) NOT NULL,
     date DATE, 
     price FLOAT, 
-    store VARCHAR(1000)
+    store VARCHAR(1000),
+    --
+    product_id INT
+);
+
+------------------ 
+
+CREATE TABLE IF NOT EXISTS prod_feat (
+    feature_id INT NOT NULL,
+    product_id INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS feature (
+    feature_id INT NOT NULL PRIMARY KEY,
+    feature_label VARCHAR(400) NOT NULL,
+    feature_type VARCHAR(400)
 );
